8c16ade40d6556f1234ed805c6b9edf6138895f6
Bryce Kerley <bkerley@brycekerley.net>
2008-02-09 16:33:11 -0500

    post passes validation tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
 	has_and_belongs_to_many :tag
+	validates_format_of :slug, :with=>/(![.\/:])/
+	validates_presence_of :slug, :title
+	validates_uniqueness_of :slug, :title
 end
