6cff57e6958a4d36563b8f52f9dbc2cd45fd1c23
Bryce Kerley <bkerley@brycekerley.net>
2008-02-09 16:35:01 -0500

    failing tag test

--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -2,4 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TagTest < ActiveSupport::TestCase
 	should_have_and_belong_to_many :post
+	
+	should_require_unique_attributes :title
+	should_require_attributes :title
 end
