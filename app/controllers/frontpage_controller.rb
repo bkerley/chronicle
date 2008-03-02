a29b152b86fde819b3bd8e5da3cf896ae054835c
Bryce Kerley <bkerley@brycekerley.net>
2008-03-02 16:57:11 -0500

    basic tags page

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -3,7 +3,15 @@ class FrontpageController < ApplicationController
   def index
 		@posts = Post.find(:all, :order=>'created_at desc', :limit=>5)
   end
-
+	
+	def date
+		
+	end
+	
+	def tags
+		@tags = Tag.find(:all)
+	end
+	
   def single
 		@post = Post.find_by_slug(params[:slug])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
-	map.resources :tags
-	
-	map.resources :posts
-	
 	map.frontpage '', :controller=>'frontpage', :action=>'index', :page=>1
 	map.archive 'page/:page', :controller=>'frontpage', :action=>'index'
 	map.single 'post/:slug', :controller=>'frontpage', :action=>'single'
+	map.tags 'tags', :controller=>'frontpage', :action=>'tags'
+	map.date 'date', :controller=>'frontpage', :action=>'date'
+	
+	
+	map.resources :tags
+	
+	map.resources :posts
+
 	
 	# See how all your routes lay out with "rake routes"
 	# Install the default routes as the lowest priority.
