6f81a614c49ce664c26d815f584917582ca98bcb
Bryce Kerley <bkerley@brycekerley.net>
2008-03-15 14:47:27 -0400

    authentication inclusion

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,4 +7,6 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'ba200ed6db47401e9bc9d5ea6d8ccadc'
+	
+	include AuthenticatedSystem
 end
