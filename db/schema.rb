5ee8ba7973000e5db708466b87c27cb654f095aa
Bryce Kerley <bkerley@brycekerley.net>
2008-05-22 22:45:58 -0400

    atom schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 5) do
+ActiveRecord::Schema.define(:version => 6) do
+
+  create_table "atoms", :force => true do |t|
+    t.string   "url"
+    t.string   "name"
+    t.datetime "updated"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
