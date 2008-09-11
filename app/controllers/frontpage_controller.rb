35d407b228f054347e554083f08a3cc9d7e3cdfd
Bryce Kerley <bkerley@brycekerley.net>
2008-09-10 21:55:21 -0400

    fixed restfulness of frontpage

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -15,22 +15,27 @@ class FrontpageController < ApplicationController
 		@posts = Tag.find_by_title(params[:tag]).posts.find(:all, :order=>'created_at desc') rescue
 			raise(ActiveRecord::RecordNotFound, "No posts with that tag")
 		self.title = "Posts tagged #{params[:tag]}"
-		render :template=>'frontpage/index'
+		respond_to do |format|
+			format.html { render :template=>'frontpage/index' }
+			format.atom { render :format=>'atom', :controller=>'posts', :action=>'index'}
+		end
 	end
 	
 	def tags
 		tags = Tag.find(:all, :order=>'title asc')
 		@tag_cloud = tags.select{|t| t.posts.length > 0}.map{|t| {:title=>t.title, :count=>t.posts.length}}
 		self.title = "Tags"
+		respond_to do |wants|
+			wants.html
+		end
 	end
 	
   def single
 		@post = Post.find_by_slug(params[:slug]) or 
 			raise( ActiveRecord::RecordNotFound, "No posts with that slug" )
 		self.title = @post.title
-		respond_to do |format|
-			format.html
+		respond_to do |wants|
+			wants.html
 		end
-		
   end
 end
